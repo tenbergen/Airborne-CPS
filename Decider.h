669e4e577d917ed67ef48d486d10bccc44a384d2
Steven Spier <sspier@oswego.edu>
2016-12-03 13:56:49 -0500

    Addded sense resolution with altitude crossing

--- Decider.h
+++ Decider.h
@@ -11,13 +11,14 @@ public:
 	Decider(Aircraft* thisAircraft);
 	void Analyze(Aircraft* intruder);
 	enum Sense { UPWARD, DOWNWARD, MAINTAIN };
-	enum Strength { CLIMB, CROSSING_CLIMB, MAINTAIN_CLIMB, DO_NOT_DESCEND_500, DO_NOT_DESCEND_1000, DO_NOT_DESCEND_2000 };
+	enum Strength { CLIMB, MAINTAIN_CLIMB, DO_NOT_DESCEND_500, DO_NOT_DESCEND_1000, DO_NOT_DESCEND_2000, 
+		DESCEND, MAINTAIN_DESCEND, DO_NOT_CLIMB_500, DO_NOT_CLIMB_1000, DO_NOT_CLIMB_2000 };
 
 private:
 	static Distance const kProtectionVolumeRadius_;
 	Aircraft* thisAircraft_;
-	double taThreshold = 60.0; // seconds
-	double raThreshold = 30.0; // seconds
+	double taThreshold = 60.0; //seconds
+	double raThreshold = 30.0; //seconds
 	void DetermineActionRequired(Aircraft* intruder);
 	double CalculateVerticalSeparation(double thisAircraftsAltitude, double intrudersAltitude);
 	double CalculateRate(double separation, double temp, time_t elapsedTime);

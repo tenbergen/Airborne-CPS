adcc821c2b88a9750bea36da31a602206de05bec
Steven Spier <sspier@oswego.edu>
2016-12-10 16:43:21 -0500

    Added strength calc and recommendation range setting

--- Decider.cpp
+++ Decider.cpp
@@ -54,46 +54,58 @@ void Decider::DetermineActionRequired(Aircraft* intruder) {
 	double taElapsedTime = Decider::CalculateElapsedTime(Decider::ToMinutes(thisAircraft_->position_old_time_),
 		Decider::ToMinutes(thisAircraft_->position_current_time_));
 
-	double horizontalRate = Decider::CalculateRate(currHorizontalSeparation, prevHorizontalSeparation, taElapsedTime);
-	double horizontalTau = Decider::CalculateTau(currHorizontalSeparation, horizontalRate);
-	double verticalRate = Decider::CalculateRate(currVerticalSeparation, prevVerticalSeparation, taElapsedTime);
-	double verticalTau = Decider::CalculateTau(currVerticalSeparation, verticalRate);
+	if (taElapsedTime != 0 && taCurrAltitude - taPrevAltitude != 0) {
+		double horizontalRate = Decider::CalculateRate(currHorizontalSeparation, prevHorizontalSeparation, taElapsedTime);
+		double horizontalTau = Decider::CalculateTau(currHorizontalSeparation, horizontalRate);
+		double verticalRate = Decider::CalculateRate(currVerticalSeparation, prevVerticalSeparation, taElapsedTime);
+		double verticalTau = Decider::CalculateTau(currVerticalSeparation, verticalRate);
 
-	double slantRangeRate = Decider::CalculateSlantRangeRate(horizontalRate, verticalRate, taElapsedTime);
-	double slantRangeTau = Decider::CalculateTau(currSlantRange, slantRangeRate); //Time to Closest Point of Approach (CPA)
+		double slantRangeRate = Decider::CalculateSlantRangeRate(horizontalRate, verticalRate, taElapsedTime);
+		double slantRangeTau = Decider::CalculateTau(currSlantRange, slantRangeRate); //Time to Closest Point of Approach (CPA)
 
-	double inHorizontalVelocity = inCurrPosition.Range(&inPrevPosition).to_feet() / inElapsedTime;
-	double inVerticalVelocity = intruder->vertical_velocity_.to_feet_per_min();
-	double taHorizontalVelocity = taCurrPosition.Range(&taPrevPosition).to_feet() / taElapsedTime;
-	double taVerticalVelocity = thisAircraft_->vertical_velocity_.to_feet_per_min();
+		double inHorizontalVelocity = inCurrPosition.Range(&inPrevPosition).to_feet() / inElapsedTime;
+		double inVerticalVelocity = intruder->vertical_velocity_.to_feet_per_min();
+		double taHorizontalVelocity = taCurrPosition.Range(&taPrevPosition).to_feet() / taElapsedTime;
+		double taVerticalVelocity = thisAircraft_->vertical_velocity_.to_feet_per_min();
 
-	Aircraft::ThreatClassification threat_class;
-	ResolutionConnection* connection = active_connections[intruder->id_];
-	if (currSlantRange < kProtectionVolumeRadius_.to_feet()) {
-		if (horizontalTau <= raThreshold && verticalTau <= raThreshold) {
-			threat_class = Aircraft::ThreatClassification::RESOLUTION_ADVISORY;
-		} else if (horizontalTau <= taThreshold && verticalTau <= taThreshold) {
-			threat_class = Aircraft::ThreatClassification::TRAFFIC_ADVISORY;
+		Aircraft::ThreatClassification threat_class;
+		ResolutionConnection* connection = active_connections[intruder->id_];
+		if (currSlantRange < kProtectionVolumeRadius_.to_feet()) {
+			if (horizontalTau <= raThreshold && verticalTau <= raThreshold) {
+				threat_class = Aircraft::ThreatClassification::RESOLUTION_ADVISORY;
+			} else if (horizontalTau <= taThreshold && verticalTau <= taThreshold) {
+				threat_class = Aircraft::ThreatClassification::TRAFFIC_ADVISORY;
+			} else {
+				threat_class = Aircraft::ThreatClassification::PROXIMITY_INTRUDER_TRAFFIC;
+			}
 		} else {
-			threat_class = Aircraft::ThreatClassification::PROXIMITY_INTRUDER_TRAFFIC;
+			threat_class = Aircraft::ThreatClassification::NON_THREAT_TRAFFIC;
+			if (connection) {
+				delete connection;
+				active_connections[intruder->id_] = NULL;
+			}
 		}
-	} else {
-		threat_class = Aircraft::ThreatClassification::NON_THREAT_TRAFFIC;
-		if (connection) {
-			delete connection;
-			active_connections[intruder->id_] = NULL;
+		char debug_buf[256];
+		snprintf(debug_buf, 256, "Decider::DetermineActionRequired - intruderId: %s, currentSlantRange: %.3f, horizontalTau: %.3f, verticalTau: %.3f, threat_class: %s \n", intruder->id_.c_str(), currSlantRange, horizontalTau, verticalTau, get_threat_class_str(threat_class).c_str());
+		XPLMDebugString(debug_buf);
+
+		Sense sense = Decider::DetermineResolutionSense(taCurrAltitude, taVerticalVelocity, inVerticalVelocity, slantRangeTau);
+		double strength = Decider::DetermineStrength(taVerticalVelocity, inVerticalVelocity, sense, slantRangeTau);
+
+		recommendation_range_lock_.lock();
+		if (sense == Sense::UPWARD) {
+			positive_recommendation_range_.min_vertical_speed.to_feet_per_min = strength;
+			negative_recommendation_range_.max_vertical_speed.to_feet_per_min = 4000;
+		} else if (sense == Sense::DOWNWARD) {
+			positive_recommendation_range_.min_vertical_speed.to_feet_per_min = strength;
+			negative_recommendation_range_.max_vertical_speed.to_feet_per_min = -4000;
 		}
+		recommendation_range_lock_.unlock();
+
+		intruder->lock_.lock();
+		intruder->threat_classification_ = threat_class;
+		intruder->lock_.unlock();
 	}
-	char debug_buf[256];
-	snprintf(debug_buf, 256, "Decider::DetermineActionRequired - intruderId: %s, currentSlantRange: %.3f, horizontalTau: %.3f, verticalTau: %.3f, threat_class: %s \n", intruder->id_.c_str(), currSlantRange, horizontalTau, verticalTau, get_threat_class_str(threat_class).c_str());
-	XPLMDebugString(debug_buf);
-
-	Sense sense = Decider::DetermineResolutionSense(taCurrAltitude, taVerticalVelocity, inVerticalVelocity, slantRangeTau);
-	Strength strength = Decider::DetermineStrength(taVerticalVelocity, inVerticalVelocity, sense, slantRangeTau);
-
-	intruder->lock_.lock();
-	intruder->threat_classification_ = threat_class;
-	intruder->lock_.unlock();
 }
 
 Sense Decider::DetermineResolutionSense(double taCurrAlt, double taVV, double inVV, double slantRangeTau) {
@@ -119,35 +131,17 @@ Sense Decider::DetermineResolutionSense(double taCurrAlt, double taVV, double in
 	return sense;
 }
 
-Decider::Strength Decider::DetermineStrength(double taVV, double inVV, Sense sense, double slantRangeTau) {
+double Decider::DetermineStrength(double taVV, double inVV, Sense sense, double slantRangeTau) {
 	double taVertProj = taVV * slantRangeTau;
 	double inVertProj = inVV * slantRangeTau;
 
 	if (sense == Sense::UPWARD) {
-		if (taVertProj >= inVertProj + 3000) { return Strength::MAINTAIN_CLIMB;}
-		else {
-			double minReqVelocity_CLIMB = inVertProj + 3000 / slantRangeTau;
-			if (minReqVelocity_CLIMB > 0 && minReqVelocity_CLIMB <= 1000) { return Strength::CLIMB_500;} 
-			else if (minReqVelocity_CLIMB > 1000 && minReqVelocity_CLIMB <= 1500) { return  Strength::CLIMB_1000; }
-			else if (minReqVelocity_CLIMB > 1500 && minReqVelocity_CLIMB <= 2000) { return Strength::CLIMB_1500; }
-			else if (minReqVelocity_CLIMB > 2000 && minReqVelocity_CLIMB <= 2500) { return Strength::CLIMB_2000; }
-			else if (minReqVelocity_CLIMB > 2500 && minReqVelocity_CLIMB <= 3000) { return Strength::CLIMB_2500; }
-			else if (minReqVelocity_CLIMB > 3000 && minReqVelocity_CLIMB <= 3500) { return Strength::CLIMB_3000; }
-			else if (minReqVelocity_CLIMB > 3500) { return Strength::CLIMB_3500; }
-		}
+		if (taVertProj >= inVertProj + 3000) { return taVV; }
+		else {return inVertProj + 3000 / slantRangeTau; }
 	} else if (sense == Sense::DOWNWARD) {
-		if (taVertProj <= inVertProj - 3000) { return Strength::MAINTAIN_DESCEND; }
-		else {
-			double minReqVelocity_DESCEND = inVertProj - 3000 / slantRangeTau;
-			if (minReqVelocity_DESCEND < 0 && minReqVelocity_DESCEND >= -1000) { return Strength::DESCEND_500; }
-			else if (minReqVelocity_DESCEND < -1000 && minReqVelocity_DESCEND >= -1500) { return Strength::DESCEND_1000; }
-			else if (minReqVelocity_DESCEND < -1500 && minReqVelocity_DESCEND >= -2000) { return Strength::DESCEND_1500; }
-			else if (minReqVelocity_DESCEND < -2000 && minReqVelocity_DESCEND >= -2500) { return Strength::DESCEND_2000; }
-			else if (minReqVelocity_DESCEND < -2500 && minReqVelocity_DESCEND >= -3000) { return Strength::DESCEND_2500; }
-			else if (minReqVelocity_DESCEND < -3000 && minReqVelocity_DESCEND >= -3500) { return Strength::DESCEND_3000; }
-			else if (minReqVelocity_DESCEND < -3500) { return Strength::DESCEND_3500; }
-		}
-	} else { return Strength::UNKNOWN_STRENGTH; }
+		if (taVertProj <= inVertProj - 3000) { return taVV; }
+		else { return inVertProj - 3000 / slantRangeTau; }
+	} else { return taVV; }
 }
 
 
@@ -170,7 +164,7 @@ double Decider::CalculateRate(double separation1, double separation2, double ela
 }
 
 double Decider::CalculateTau(double separation, double rate) {
-	return separation / rate;
+		return separation / rate;
 }
 
 double Decider::CalculateSlantRange(double horizontalSeparation, double verticalSeparation) {
--- Decider.h
+++ Decider.h
@@ -13,19 +13,6 @@ class Decider {
 public:
 	Decider(Aircraft* thisAircraft);
 	void Analyze(Aircraft* intruder);
-	//enum Strength {
-	//	CLIMB, MAINTAIN_CLIMB, DO_NOT_DESCEND_500, DO_NOT_DESCEND_1000, DO_NOT_DESCEND_2000,
-	//	DESCEND, MAINTAIN_DESCEND, DO_NOT_CLIMB_500, DO_NOT_CLIMB_1000, DO_NOT_CLIMB_2000
-	//};
-
-	enum Strength {
-		CLIMB, MAINTAIN_CLIMB, CLIMB_500, CLIMB_1000, CLIMB_1500, CLIMB_2000, CLIMB_2500, CLIMB_3000, CLIMB_3500, CLIMB_4000,
-		DESCEND, MAINTAIN_DESCEND, DESCEND_500, DESCEND_1000, DESCEND_1500, DESCEND_2000, DESCEND_2500, DESCEND_3000, DESCEND_3500, DESCEND_4000,
-		UNKNOWN_STRENGTH
-	};
-
-
-
 	std::mutex recommendation_range_lock_;
 	RecommendationRange positive_recommendation_range_;
 	RecommendationRange negative_recommendation_range_;
@@ -46,6 +33,6 @@ private:
 	double CalculateElapsedTime(double t1, double t2);
 	Sense DetermineResolutionSense(double thisAircraftCurrentAltitude, double thisAircraftsVerticalVelocity,
 		double intruderVerticalVelocity, double slantRangeTau);
-	Strength DetermineStrength(double taVerticalVelocity, double inVerticalVelocity, Sense s, double slantRangeTau);
+	double DetermineStrength(double taVerticalVelocity, double inVerticalVelocity, Sense s, double slantRangeTau);
 	concurrency::concurrent_unordered_map<std::string, ResolutionConnection*> active_connections;
 };
